95ed3412265dac1d7a1576291df58711912985e5
Stephan Balmer <sb@cis.ch>
2016-11-30 03:37:05 +0100

    fix region predicate

--- routing.js
+++ routing.js
@@ -143,16 +143,15 @@ Router.route('/profile/unsubscribe/:token', function() {
 
 
 Router.map(function () {
-  this.route('json0Venues', {
-    path: '/json/0/venues',
-    where: 'server',
-    action: function () {
-		var venueQuery = Filtering(VenuePredicates).read(this.params.query).toQuery();
+	this.route('json0Venues', {
+		path: '/json/0/venues',
+		where: 'server',
+		action: function () {
+			var venueQuery = Filtering(VenuePredicates).read(this.params.query).done().toQuery();
+			var venues = Venues.find(venueQuery).fetch();
 
-		var venues = Venues.find(venueQuery).fetch();
-
-		this.response.setHeader('Content-Type', 'application/json');
-		this.response.end(JSON.stringify(venues));
-  }
-});
+			this.response.setHeader('Content-Type', 'application/json');
+			this.response.end(JSON.stringify(venues));
+		}
+	});
 });
